69b24d2c489d4b1e09a6c80d3bf7f44614169a61
unknown <s223603@student.uek.krakow.pl>
2023-10-25 18:41:12 +0200

    25/10 day 3

--- interface.py
+++ interface.py
@@ -1,36 +1,43 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 
 from downloader import Downloader
 
 dnld = Downloader()
 dictionary = dnld.extractor()
-root = Tk()
-
 
 def display():
     for termin, godziny in dictionary.items():
-        label_termin = ttk.Label(frame_daty, text=termin, width=12, anchor='w')
-        label_termin.grid(sticky='w')
-        #(sticky='w', padx=(0, 10)))
-        #i += f"{termin}\n "
+        label_termin = ttk.Label(
+            frame_daty,
+            text=termin,)
+        label_termin.pack()
         for godzina in godziny:
-            label_godziny = ttk.Label(frame_daty, text=godzina, width=12, anchor='w')
-            label_godziny.grid(sticky='w')
-        #i += f" \n"
+            label_godziny = ttk.Label(
+                frame_daty,
+                text=godzina,
+                font=("Helvetica", 16))
+            label_godziny.pack()
+
+# window
+window = tk.Tk()
+window.geometry('600x400')
+window.title("Kalendarz")
+
+# Date frames
+frame_daty = ttk.Frame(window, width=200, height=200, borderwidth=10, relief=tk.GROOVE)
+frame_daty.pack()
+
+# Exit button
+b_exit = ttk.Button(window, text="Wyjdz", command=window.destroy)
+b_exit.pack()
+
+# Load button
+b_load = ttk.Button(window, text="Zapodaj", command=lambda: display())
+b_load.pack()
 
 
 
 
-root.title("Kalendarz")
-frame_daty = ttk.Frame(root)
-b_exit = ttk.Button(root, text="Wyjdz",command=root.destroy)
-b_load = ttk.Button(root, text="Zapodaj",command=lambda: display())
-label_disp = ttk.Label(root, text="Tutaj wyświetlą się terminy")
 
-b_exit.grid(column=0,row=0)
-b_load.grid(column=1,row=1)
-frame_daty.grid()
-
-
-root.mainloop()
+window.mainloop()
